9bac787ff8924f18b01ada9922535cd5b1d01cb8
xReflake <79257338+xReflake@users.noreply.github.com>
2021-06-17 11:42:05 +0300

    Add files via upload

---
+++ csauto.py
@@ -0,0 +1,89 @@
+import os
+import time
+xq = 0
+#Запуск
+print ('Введите номер пачки: ')
+pak = int(input()) #Переменная пака
+file = open('C:\Packs\Pack{0}.txt'.format(pak))
+line1 = file.readline() #Берем 1 строку
+line1 = line1.rstrip() #Удаляем /n
+log = str('{0}'.format(line1))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('1-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+
+time.sleep(xq)
+line2 = file.readline()
+line2 = line2.rstrip()
+log = str('{0}'.format(line2))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('2-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+
+time.sleep(xq)
+line3 = file.readline()
+line3 = line3.rstrip()
+log = str('{0}'.format(line3))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('3-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+
+time.sleep(xq)
+line4 = file.readline()
+line4 = line4.rstrip()
+log = str('{0}'.format(line4))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('4-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+
+time.sleep(xq)
+line5 = file.readline()
+line5 = line5.rstrip()
+log = str('{0}'.format(line5))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('5-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+
+time.sleep(xq)
+line6 = file.readline()
+line6 = line6.rstrip()
+log = str('{0}'.format(line6))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('6-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+
+time.sleep(xq)
+line7 = file.readline()
+line7 = line7.rstrip()
+log = str('{0}'.format(line7))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('7-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+
+time.sleep(xq)
+line8 = file.readline()
+line8 = line8.rstrip()
+log = str('{0}'.format(line8))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('8-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+
+time.sleep(xq)
+line9 = file.readline()
+line9 = line9.rstrip()
+log = str('{0}'.format(line9))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('9-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+
+time.sleep(xq)
+line10 = file.readline()
+line10 = line10.rstrip()
+log = str('{0}'.format(line10))
+zapusk = str(r'C:\Windows\system32\cmd.exe /C Start C:\"Program Files (x86)"\Steam\steam.exe -login {0} steam://rungameid/730'.format(log))
+print('10-й аккаунт успешно запущен.')
+os.system('{0}'.format(zapusk))
+time.sleep(xq)
+uopilotzap = str(r'C:\uopilot\uopilot.exe')
+
+
